93fad082132b74ab368f36e543b6085ec3a03c62
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-03-01 19:48:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,45 +1,45 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-bool checkPrime(uint64_t val) {
-// вставьте код функции
-if (val <= 1) {
-    return false;
-    }
-for (uint64_t i = 2; i*i <= val; i++) {
-    if (val % i == 0) {
-        return false;
-    }
+#include <cmath>
+bool checkPrime(uint64_t value) {
+if (value <= 1) {
+return false;
 }
-    return true;
+for (uint64_t i = 2; i <= sqrt(value); i++) {
+if (value % i == 0) {
+return false;
+}
+}
+return true;
 }
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
-  uint64_t idx = 0;
-  uint64_t cur_p = 1;
-    while (idx < n) {
-        cur_p++;
-        if (checkPrime(cur_p)) {
-            idx++;
-        }
-    }
-return cur_p;                                                                                                                                                                                                                                                          
-}          
+if (n == 1) {
+return 2;
+}
+int64_t count = 1;
+uint64_t cur_p = 3;
+while (count < n) {
+cur_p += 2;
+if (checkPrime(cur_p)) {
+count++;
+}
+}
+return cur_p;
+}
 uint64_t nextPrime(uint64_t value) {
-  value++;
-  while (!checkPrime(value)) {
-        value++;
-    }
-    return value;
+value++;
+while (!checkPrime(value)) {
+value++;
+}
+return value;
 }
 uint64_t sumPrime(uint64_t hbound) {
 uint64_t sum = 0;
-uint64_t val = 2;  
-while(val < hbound) {
-        if (checkPrime(val)) {
-            sum += val;
-        }
-        val++;
-    }
-    return sum;
+for (uint64_t i = 2; i < hbound; i++) {
+if (checkPrime(i)) {
+sum += i;
+}
+}
+return sum;
 }
