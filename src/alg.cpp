493334d1e2646d501cb3e7a80b69fd845fbdb9c0
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-03-02 13:25:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,21 +7,21 @@ return false;
 if (value == 2)
 return true;
 if (value % 2 == 0)
-return false; 
+return false;
 for (uint64_t i = 3; i*i <= value; i += 2) {
 if (value % i == 0)
 return false;
-} 
+}
 return true;
 }
 uint64_t nPrime(uint64_t n) {
 uint64_t idx = 0;
-uint64_t value = 2; 
+uint64_t value = 2;
 while (idx < n) {
 if (checkPrime(value))
 idx++;
 value++;
-} 
+}
 return value-1;
 }
 uint64_t nextPrime(uint64_t value) {
@@ -37,6 +37,6 @@ uint64_t summa = 0;
 for (uint64_t i = 2; i < hbound; i++) {
 if (checkPrime(i))
 summa += i;
-} 
+}
 return summa;
 }
